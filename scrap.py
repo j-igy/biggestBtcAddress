73c74dd640ff34960042376b70b5f08d1df9e9f4
J-jigy <jigy.deluxe@gmail.com>
2022-07-11 18:25:18 +0200

    correct btc address

--- scrap.py
+++ scrap.py
@@ -1,14 +1,15 @@
 # get block-chain info about the biggest 
-# Bitcoin Address bc1qgdjqv0av3q56jvd82tkdjpy7gdp9ut8tlqmgrpmv24sq90ecnvqqjwvw97
+# Bitcoin Address 1P5ZEDWTKTFGxQjZphgWPQUpe554WKDfHQ
 # from bitinfocharts.com page
 
+from datetime import datetime
 import pandas as pd
 import ssl
 import requests
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-url      = "https://bitinfocharts.com/bitcoin/address/bc1qgdjqv0av3q56jvd82tkdjpy7gdp9ut8tlqmgrpmv24sq90ecnvqqjwvw97"
+url      = "https://bitinfocharts.com/bitcoin/address/1P5ZEDWTKTFGxQjZphgWPQUpe554WKDfHQ"
 headers  = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"}
 resp     = requests.get(url, headers=headers)
 dfTables = pd.read_html(resp.text)
@@ -16,4 +17,4 @@ dfTable  = dfTables[2]
 dfTable  = dfTable.drop(["Block","Balance","Balance, USD @ Price", "Profit"], axis=1)
 
 # dfTable.to_csv("test.csv")
-print(dfTable)
+# print(dfTable)
